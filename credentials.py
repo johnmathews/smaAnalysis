f62aa12f1739132d8d84084a78d8a84d545bcf7b
johnmathews <mthwsjc@gmail.com>
2017-10-30 13:46:32 +0000

    already published to blog

---
+++ credentials.py
@@ -0,0 +1,11 @@
+import quandl
+import plotly
+
+# quandl API
+quandl.ApiConfig.api_key = "sSTsxvoy-i_fotUWXYoh"
+
+# plotly API
+plotly.tools.set_credentials_file(username='mthwsjc', api_key='KKfrs90UfQsa98gg52ak')
+#plotly.tools.set_credentials_file(username='jonnosgone', api_key='xlatwGCFgZzPOzsLr9v1')
+
+
